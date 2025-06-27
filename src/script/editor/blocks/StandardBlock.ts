a00da2a72f7cfafc63ae95096a579bc469474cdf
RaftDev <theraft08@gmail.com>
2025-06-28 00:18:13 +0200

    Added some documentation on the StandardBlock

--- src/script/editor/blocks/StandardBlock.ts
+++ src/script/editor/blocks/StandardBlock.ts
@@ -2,6 +2,9 @@ import {CodeBlock} from "../CodeBlock.ts";
 import * as Blockly from "blockly";
 import type {BlockDefinition} from "blockly/core/blocks";
 
+/**
+ * Represents a block already added by blockly and thus does not need a block definition to be set for it.
+ */
 export abstract class StandardBlock extends CodeBlock {
     protected definition(): BlockDefinition {
         return Blockly.Blocks[this.identifier()];
