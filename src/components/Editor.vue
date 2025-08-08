a52f73d003a84304085f9c095b6f447748ec2a12
RaftDev <theraft08@gmail.com>
2025-08-08 17:12:55 +0200

    Added floating menu for more advanced functions such as exporting and importing

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,14 +1,76 @@
 <script setup lang="ts">
-import { NSplit } from 'naive-ui';
+import {NFloatButton, NIcon, NSplit, NTooltip} from 'naive-ui';
 import BlockEditor from "./editor/BlockEditor.vue";
 import CodeViewer from "./editor/CodeViewer.vue";
 import {useEditorStore} from "../script/editor/EditorStore.ts";
+import {Menu2 as MenuIcon, Files as CopyIcon, CircleCheck as CopiedIcon, FileCode as ExportCodeIcon, FileDownload as DownloadIcon, FileImport as ImportIcon, FileShredder as ClearIcon } from '@vicons/tabler'
+import {ref} from "vue";
 
 const editorStore = useEditorStore();
+
+const codeCopied = ref(false);
+async function onCopyClick() {
+  try {
+    await navigator.clipboard.writeText(editorStore.code)
+    codeCopied.value = true
+    setTimeout(() => {
+      codeCopied.value = false
+    }, 1500)
+  } catch (e) {
+    console.error('Failed to copy text: ', e)
+  }
+}
 
 </script>
 
 <template>
+  <n-float-button :right="24" :bottom="24" :width="56" :height="56" shape="circle" menu-trigger="click" type="primary">
+    <n-icon size="36"><MenuIcon /></n-icon>
+    <template #menu>
+      <n-tooltip trigger="hover" placement="left">
+        <template #trigger>
+          <n-float-button :width="48" :height="48" shape="circle" @click="onCopyClick">
+            <n-icon v-if="!codeCopied" size="24"><CopyIcon/></n-icon>
+            <n-icon v-if="codeCopied" size="24"><CopiedIcon/></n-icon>
+          </n-float-button>
+        </template>
+        <span v-if="!codeCopied">Copy</span>
+        <span v-if="codeCopied">Copied!</span>
+      </n-tooltip>
+      <n-tooltip trigger="hover" placement="left">
+        <template #trigger>
+          <n-float-button :width="48" :height="48" shape="circle">
+            <n-icon size="24"><ExportCodeIcon/></n-icon>
+          </n-float-button>
+        </template>
+        Export to code
+      </n-tooltip>
+      <n-tooltip trigger="hover" placement="left">
+        <template #trigger>
+          <n-float-button :width="48" :height="48" shape="circle">
+            <n-icon size="24"><DownloadIcon/></n-icon>
+          </n-float-button>
+        </template>
+        Download Project
+      </n-tooltip>
+      <n-tooltip trigger="hover" placement="left">
+        <template #trigger>
+          <n-float-button :width="48" :height="48" shape="circle">
+            <n-icon size="24"><ImportIcon/></n-icon>
+          </n-float-button>
+        </template>
+        Import Project
+      </n-tooltip>
+      <n-tooltip trigger="hover" placement="left">
+        <template #trigger>
+          <n-float-button :width="48" :height="48" shape="circle" style="background-color: #fd2a2a">
+            <n-icon size="24"><ClearIcon style="color: white"/></n-icon>
+          </n-float-button>
+        </template>
+        Delete Project
+      </n-tooltip>
+    </template>
+  </n-float-button>
   <n-split direction="horizontal" id="editor-split" v-model:size="editorStore.dividerPos" :min="0.5" :max="0.8">
     <template #1>
       <BlockEditor/>
--- src/components/editor/CodeViewer.vue
+++ src/components/editor/CodeViewer.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { NCode, NSpace } from 'naive-ui';
-import {useEditorStore} from "../../script/editor/EditorStore.ts";
+import { NCode } from 'naive-ui';
+import { useEditorStore } from "../../script/editor/EditorStore.ts";
 
 const editorStore = useEditorStore();
 
@@ -8,8 +8,10 @@ const editorStore = useEditorStore();
 
 <template>
   <div style="margin: 15px">
-    <n-code show-line-numbers language="cpp" :code="editorStore.code">
-
-    </n-code>
+    <n-code show-line-numbers language="cpp" :code="editorStore.code"/>
   </div>
 </template>
+
+<style>
+
+</style>
