cd0b4edebb1c463393db0f5287cac1ec01b8716d
RaftDev <theraft08@gmail.com>
2025-08-14 00:09:25 +0200

    New Toolbox category system, allows for sub-categories to be created.

--- src/script/editor/Categories.ts
+++ src/script/editor/Categories.ts
@@ -1,36 +1,64 @@
 import type {CategoryInfo, ToolboxItemInfo} from "blockly/core/utils/toolbox";
+import type {CssConfig as CategoryCssConfig} from "blockly/core/toolbox/category";
 
-function registerCategory(name: string, color: number): CategoryInfo {
-    return {
-        kind: 'category',
-        name: name,
-        colour: color
-    }
+const toolbox: ToolboxItemInfo[] = [];
+
+function createRootCategory(name: string, color: number, appendSpace: boolean = false): Category {
+    const category: Category = new Category(name, color);
+
+    toolbox.push(category);
+    if (appendSpace)
+        toolbox.push(createSpacing());
+
+    return category;
 }
 
 function createSpacing(): ToolboxItemInfo {
     return { kind: 'sep' };
 }
 
+class Category implements CategoryInfo {
+    kind: 'category';
+    name: string;
+    colour: number;
+    contents: ToolboxItemInfo[];
+    id: string | undefined;
+    categorystyle: string | undefined;
+    cssconfig: CategoryCssConfig | undefined;
+    hidden: string | undefined;
+    expanded?: string | boolean;
+
+    constructor(name: string, colour: number) {
+        this.kind = 'category';
+        this.name = name;
+        this.colour = colour;
+        this.contents = [];
+
+        // Blockly managed properties
+        this.id = undefined;
+        this.categorystyle = undefined;
+        this.cssconfig = undefined;
+        this.hidden = undefined;
+        this.expanded = undefined;
+    }
+
+    public createSubDirectory(name: string, color: number): Category {
+        const subCategory = new Category(name, color);
+        this.contents.push(subCategory);
+        return subCategory;
+    }
+}
+
 export class Categories {
-    static readonly CORE = registerCategory('Core', 60);
-    static readonly LOGIC = registerCategory('Logic', 120);
-    static readonly MATH = registerCategory('Math', 180);
-    static readonly VARIABLE = registerCategory('Variable', 230);
+    static readonly CORE = createRootCategory('Core', 60);
+    static readonly LOGIC = createRootCategory('Logic', 120);
+    static readonly MATH = createRootCategory('Math', 180);
+    static readonly VARIABLE = createRootCategory('Variable', 230, true);
 
     // Module Specific
-    static readonly M5STACK = registerCategory('M5Stack', 360);
+    static readonly M5STACK = createRootCategory('M5Stack', 360);
 
     static asToolboxContent(): ToolboxItemInfo[] {
-        const content: ToolboxItemInfo[] = [
-            Categories.CORE,
-            Categories.LOGIC,
-            Categories.MATH,
-            Categories.VARIABLE,
-            createSpacing(),
-            Categories.M5STACK,
-        ];
-
-        return content;
+        return toolbox;
     }
 }
