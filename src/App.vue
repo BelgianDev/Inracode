60a9e26b013ac693b969458401fb19fd8c3d8318
RaftDev <theraft08@gmail.com>
2025-12-19 13:02:27 +0100

    Prevent smaller devices from loading the page and breaking the app layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import { NConfigProvider, NDialogProvider, NModalProvider } from 'naive-ui';
 import Editor from "./components/Editor.vue";
 
 import hljs from "highlight.js";
 import cpp from "highlight.js/lib/languages/c";
+import Phone from "./components/pages/Phone.vue";
+
+
+const MIN_SUPPORTED_WIDTH = 768 // Minimum allowed resolution width for the editor to work properly.
+const validResolution = ref<boolean>(true)
+
+function handleResize() {
+  const size = typeof window !== 'undefined' ? window.innerWidth : MIN_SUPPORTED_WIDTH
+  validResolution.value = size >= MIN_SUPPORTED_WIDTH
+}
+
+onMounted(() => {
+  handleResize()
+  window.addEventListener('resize', handleResize, { passive: true })
+})
 
 // Code Highlight
 hljs.registerLanguage('cpp', cpp);
@@ -13,7 +29,8 @@ hljs.registerLanguage('cpp', cpp);
   <n-config-provider :hljs="hljs">
     <n-modal-provider>
       <n-dialog-provider>
-        <Editor/>
+        <Phone v-if="!validResolution"/>
+        <Editor v-else/>
       </n-dialog-provider>
     </n-modal-provider>
   </n-config-provider>
---
+++ src/components/pages/Phone.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {NIcon} from "naive-ui";
+import {
+  DeviceDesktopOff as PhoneIcon,
+} from '@vicons/tabler'
+
+</script>
+
+<template>
+    <div id="phone-page">
+      <n-icon class="phone-icon" size="124"><PhoneIcon/></n-icon>
+      <h3>This website is not supported on smaller devices.</h3>
+      <p>Use a desktop computer for the best experience.</p>
+    </div>
+</template>
+
+<style scoped>
+#phone-page {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  color: rgb(172, 172, 172);
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
