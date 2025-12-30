7026ab65c307b23dc72512200b5ec4d97a70baff
RaftDev <theraft08@gmail.com>
2025-12-30 02:12:21 +0100

    Improved for I loop

--- src/script/editor/blocks/logic/ForILoopBlock.ts
+++ src/script/editor/blocks/logic/ForILoopBlock.ts
@@ -1,14 +1,19 @@
-import {StandardBlock} from "../StandardBlock.ts";
 // @ts-ignore
 import type {CategoryInfo} from "blockly/core/utils/toolbox";
 import * as Blockly from 'blockly/core';
 import {Categories} from "../../Categories.ts";
 import {Order} from "blockly/javascript";
+import {CodeBlock} from "../../CodeBlock.ts";
+// @ts-ignore
+import type {BlockDefinition} from "blockly/core/blocks";
 
-export class ForILoopBlock extends StandardBlock {
-    public static readonly IDENTIFIER: string = "controls_repeat_ext";
-    public static readonly TIMES: string = "TIMES";
-    public static readonly STATEMENT: string = "DO";
+export class ForILoopBlock extends CodeBlock {
+    public static readonly IDENTIFIER: string = "for-i";
+    public static readonly VARIABLE: string = "VARIABLE";
+    public static readonly INITIAL_VALUE: string = "INITIAL_VALUE";
+    public static readonly CONDITION: string = "CONDITION";
+    public static readonly MODIFIER: string = "MODIFIER";
+    public static readonly STATEMENT: string = "STATEMENT";
 
     public identifier(): string {
         return ForILoopBlock.IDENTIFIER;
@@ -18,10 +23,35 @@ export class ForILoopBlock extends StandardBlock {
         return Categories.LOGIC;
     }
 
+    protected definition(): BlockDefinition {
+        return {
+            init: function () {
+                this.appendValueInput(ForILoopBlock.VARIABLE)
+                    .appendField('for variable');
+                this.appendValueInput(ForILoopBlock.INITIAL_VALUE)
+                    .appendField('with value');
+                this.appendValueInput(ForILoopBlock.CONDITION)
+                    .appendField('while');
+                this.appendValueInput(ForILoopBlock.MODIFIER)
+                    .appendField('apply');
+                this.appendStatementInput(ForILoopBlock.STATEMENT);
+                this.setInputsInline(true)
+
+                this.setPreviousStatement(true, null);
+                this.setNextStatement(true, null);
+                this.setTooltip('');
+                this.setHelpUrl('');
+            }
+        }
+    }
+
     protected generateCode(block: Blockly.Block, generator: Blockly.CodeGenerator): string | [string, number] {
-        const times = generator.valueToCode(block, ForILoopBlock.TIMES, Order.NONE);
+        const variable = generator.valueToCode(block, ForILoopBlock.VARIABLE, Order.NONE);
+        const initialValue = generator.valueToCode(block, ForILoopBlock.INITIAL_VALUE, Order.NONE);
+        const condition = generator.valueToCode(block, ForILoopBlock.CONDITION, Order.NONE);
+        const modifier = generator.valueToCode(block, ForILoopBlock.MODIFIER, Order.NONE);
         const statement = generator.statementToCode(block, ForILoopBlock.STATEMENT);
 
-        return "for (let i = 0; i < " + times + "; i++) {\n" + statement + "\n}"
+        return `for (${variable} = ${initialValue}; ${condition}; ${modifier}) {\n` + statement + '\n}';
     }
 }
