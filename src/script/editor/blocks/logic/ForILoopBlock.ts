299360c056e36b7634675f36ade31b76058066ff
RaftDev <theraft08@gmail.com>
2025-12-30 15:34:48 +0100

    Filled for loop entries with default values

--- src/script/editor/blocks/logic/ForILoopBlock.ts
+++ src/script/editor/blocks/logic/ForILoopBlock.ts
@@ -6,6 +6,10 @@ import {Order} from "blockly/javascript";
 import {CodeBlock} from "../../CodeBlock.ts";
 // @ts-ignore
 import type {BlockDefinition} from "blockly/core/blocks";
+import {NumberBlock} from "../variable/NumberBlock.ts";
+import {GetVariableBlock} from "../variable/GetVariableBlock.ts";
+import {CompareBlock} from "./CompareBlock.ts";
+import {ArithmeticBlock} from "../math/ArithmeticBlock.ts";
 
 export class ForILoopBlock extends CodeBlock {
     public static readonly IDENTIFIER: string = "for-i";
@@ -26,8 +30,9 @@ export class ForILoopBlock extends CodeBlock {
     protected definition(): BlockDefinition {
         return {
             init: function () {
-                this.appendValueInput(ForILoopBlock.VARIABLE)
-                    .appendField('for variable');
+                this.appendDummyInput('')
+                    .appendField('for')
+                    .appendField(new Blockly.FieldTextInput('i'), ForILoopBlock.VARIABLE);
                 this.appendValueInput(ForILoopBlock.INITIAL_VALUE)
                     .appendField('with value');
                 this.appendValueInput(ForILoopBlock.CONDITION)
@@ -46,12 +51,82 @@ export class ForILoopBlock extends CodeBlock {
     }
 
     protected generateCode(block: Blockly.Block, generator: Blockly.CodeGenerator): string | [string, number] {
-        const variable = generator.valueToCode(block, ForILoopBlock.VARIABLE, Order.NONE);
+        const variable = block.getFieldValue(ForILoopBlock.VARIABLE)
         const initialValue = generator.valueToCode(block, ForILoopBlock.INITIAL_VALUE, Order.NONE);
         const condition = generator.valueToCode(block, ForILoopBlock.CONDITION, Order.NONE);
         const modifier = generator.valueToCode(block, ForILoopBlock.MODIFIER, Order.NONE);
         const statement = generator.statementToCode(block, ForILoopBlock.STATEMENT);
 
         return `for (${variable} = ${initialValue}; ${condition}; ${modifier}) {\n` + statement + '\n}';
+    }
+
+    protected fillCategory(category: CategoryInfo) {
+        category.contents.push({
+            kind: 'block',
+            type: this.identifier(),
+            inputs: {
+                [ForILoopBlock.INITIAL_VALUE]: {
+                    "block": {
+                        "type": NumberBlock.IDENTIFIER,
+                        "fields": {
+                            [NumberBlock.NUM]: 0
+                        }
+                    }
+                },
+                [ForILoopBlock.CONDITION]: {
+                    "block": {
+                        "type": CompareBlock.IDENTIFIER,
+                        "inputs": {
+                            [CompareBlock.ARGUMENT_A]: {
+                                "block": {
+                                    "type": GetVariableBlock.IDENTIFIER,
+                                    "fields": {
+                                        [GetVariableBlock.VAR_NAME]: "i"
+                                    }
+                                }
+                            },
+                            [CompareBlock.ARGUMENT_B]: {
+                                "block": {
+                                    "type": NumberBlock.IDENTIFIER,
+                                    "fields": {
+                                        [NumberBlock.NUM]: 10
+                                    }
+                                }
+                            }
+                        },
+                        "fields": {
+                            [CompareBlock.OPERATOR]: "LT"
+                        }
+                    }
+                },
+                [ForILoopBlock.MODIFIER]: {
+                    "block": {
+                        "type": ArithmeticBlock.IDENTIFIER,
+                        "inputs": {
+                            [CompareBlock.ARGUMENT_A]: {
+                                "block": {
+                                    "type": GetVariableBlock.IDENTIFIER,
+                                    "fields": {
+                                        [GetVariableBlock.VAR_NAME]: "i"
+                                    }
+                                }
+                            },
+                            [CompareBlock.ARGUMENT_B]: {
+                                "block": {
+                                    "type": NumberBlock.IDENTIFIER,
+                                    "fields": {
+                                        [NumberBlock.NUM]: 1
+                                    }
+                                }
+                            }
+                        },
+                        "fields": {
+                            [ArithmeticBlock.OPERATOR]: "ADD"
+                        }
+                    }
+                },
+
+            }
+        })
     }
 }
